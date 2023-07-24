66b5ac8e6ffe51bf30df3d1185e95b6d93d00e5b
rodrigoo-soouza <digosouzafonseca@gmail.com>
2023-07-24 13:54:29 -0300

    PTI Senac

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+class Pti
+{
+    static void Main()
+    {
+        string resposta = "s";
+
+        do
+        {
+            Console.Clear();
+            Console.Write("Digite o preço da Gasolina: R$ ");
+            double gasolina = double.Parse(Console.ReadLine());
+            Console.Write("Digite o preço do Etanol: R$ ");
+            double etanol = double.Parse(Console.ReadLine());
+            double vantagem = (etanol / gasolina);
+
+            if (vantagem == 0.70)
+            {
+                Console.WriteLine("Tanto faz abastecer com Gasolina ou Etanol!");
+            }
+            else if (vantagem < 0.70)
+            {
+                Console.WriteLine("Abasteça com Etanol!");
+            }
+            else
+            {
+                Console.WriteLine("Abasteça com Gasolina!");
+            }
+            Console.Write("Deseja fazer um novo cáculo? [S] ou [N]? ");
+            resposta = Console.ReadLine().ToLower();
+        } while (resposta == "s");
+        Console.Clear();
+        Thread.Sleep(500);
+        Console.WriteLine("Encerrando o Programa...");
+        Thread.Sleep(500);
+        Console.WriteLine("Volte Sempre");
+    }
+}
